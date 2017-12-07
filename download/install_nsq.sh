df4c1d99b41b35a38a412c82eefaf4ece8130922
CHENQ <chennqqi@qq.com>
2017-12-07 09:54:52 +0800

    add nsq install

---
+++ download/install_nsq.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+targetdir="/var/nsq"
+sudo rm -f /tmp/nsq-tool.zip
+wget -P /tmp http://www.gsymy.com/download/nsq-tool.zip 
+
+if [ ! -d "$targetdir" ]; then
+	sudo unzip /tmp/nsq-tool.zip -d /var
+else
+	echo "$targetdir already exist. remove first!"
+	exit -1;
+fi
+  
